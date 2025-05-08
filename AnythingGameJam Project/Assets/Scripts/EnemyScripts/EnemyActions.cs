54a0dba0dc48c5e4bc8f22a2674ba3b94ab18d1f
Rogerium <kanekinzin360@gmail.com>
2025-05-08 15:37:06 -0300

    Finished Enemy Idle-Alert-Return States
    - fixed enemy sprite not looking at player when seen
    - fix was mostly cause by how sprites angles were calculated, instead of using the angle from the forward perspective of the enemy, i used distance calculations, which meant the angle was the same no matter the way the collider was facing.

--- AnythingGameJam Project/Assets/Scripts/EnemyScripts/EnemyActions.cs
+++ AnythingGameJam Project/Assets/Scripts/EnemyScripts/EnemyActions.cs
@@ -1,19 +1,42 @@
 using System.Runtime.CompilerServices;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class EnemyActions : MonoBehaviour
 {
     public Transform playerT;
     public Transform enemyT;
+    public Transform enemySP;
+    public LookAtPlayer lookScript;
+    public SpriteRenderer enemySprite;
     private Vector3 enemyStartPosition;
     private Quaternion enemyStartRotation;
-    private bool playerFound;
 
     Vector3 rayOrigin;
     public float rayDistance;
+    public float coneAngle;
+    public float AlertTimer;
+    private float AlertDuration = 3f;
 
+    Vector3 playerXZ;
+    Vector3 enemyXZ;
+    Vector3 direction;
+    Vector3 pDirection;
+
+    float distanceZ;
+    float distanceX;
+    float distanciaPlana;
+    float angulo;
+    float pAngulos;
+    float anguloPraPlayer;
+    public float returnSpeed = 45f;
+
+
+
+    public bool playerIsSeen;
+    bool isReturning;
+    public bool isAlert = false;
     
-
     void Start()
     {
         
@@ -24,19 +47,84 @@ public class EnemyActions : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+        pDirection = (playerT.position - transform.position).normalized;
+        anguloPraPlayer = Vector3.Angle(transform.forward, pDirection);
+
+        distanceX = playerT.position.x - transform.position.x;
+        distanceZ = playerT.position.z - transform.position.z;
+
+
+        distanciaPlana = Mathf.Sqrt(distanceX * distanceX + distanceZ * distanceZ);
+
+        direction = playerT.position - transform.position;
+        direction.y = 0;
+
+        angulo = Mathf.Atan2(distanceX, distanceZ) * Mathf.Rad2Deg;
+
+        if(angulo < 0)
+        {
+            angulo += 360;
+        }
+        pAngulos = Mathf.RoundToInt(angulo % 45);
+
+        if (isReturning)
+        {
+            transform.rotation = Quaternion.RotateTowards(
+                transform.rotation, enemyStartRotation, returnSpeed * Time.deltaTime);
+            if (Quaternion.Angle(transform.rotation, enemyStartRotation) < 0.1f)
+            {
+                transform.rotation = enemyStartRotation;
+                isReturning = false;
+                Debug.Log("retorno completo");
+            }
+        }
+
         enemyIdle();
+
+
+
     }
     
     void enemyIdle()
     {
+        playerIsSeen = false;
         rayOrigin = transform.position;
-        Debug.DrawRay(rayOrigin, transform.forward * rayDistance, Color.red);
-        playerFound = Physics.Raycast(rayOrigin, transform.forward, out RaycastHit hitInfo, rayDistance);
-        Debug.DrawRay(rayOrigin, transform.forward * rayDistance, Color.red);
+
+        
+        if (Physics.Raycast(rayOrigin, pDirection, out RaycastHit hitInfo, rayDistance))
+        {
+            
+            Debug.DrawRay(rayOrigin, pDirection * rayDistance, Color.red);
+            if (anguloPraPlayer <= coneAngle /2 &&
+                distanciaPlana <= rayDistance &&
+                hitInfo.transform == playerT)
+            {
+                playerIsSeen = true;
+                enemyAlert();
+                Debug.Log("player visto, em alerta!");
+            }
+        }
+        if(!playerIsSeen && isAlert)
+        {
+            Debug.Log("player esta fora de vista");
+            AlertTimer -= Time.deltaTime;
+            if (AlertTimer <= 0f)
+            {
+                
+                enemyReturn();
+            }
+        }
+
     }
     void enemyAlert()
     {
-
+        Debug.Log("alerta!");
+        AlertTimer = AlertDuration;
+        transform.rotation = Quaternion.LookRotation(direction);
+        isAlert = true;
+        
+        
     }
     void enemyAttack()
     {
@@ -48,6 +136,10 @@ public class EnemyActions : MonoBehaviour
     }
     void enemyReturn()
     {
-
+        isReturning = true;
+        isAlert = false;
+        
+        
+        Debug.Log("voltando ao estado inicial");
     }
 }
--- AnythingGameJam Project/Assets/Sprites/SpriteScript/LookAtPlayer.cs
+++ AnythingGameJam Project/Assets/Sprites/SpriteScript/LookAtPlayer.cs
@@ -5,36 +5,44 @@ using UnityEngine.UIElements;
 
 public class LookAtPlayer : MonoBehaviour
 {
+    public EnemyActions enemyActions;
     public Transform Player;
     public SpriteRenderer spriteRender;
     public Sprite[] sprites;
     public bool canLookVertical;
     void Update()
     {
-        
         float distanceX = Player.transform.position.x - transform.position.x;
         float distanceZ = Player.transform.position.z - transform.position.z;
 
-        float angulo = Mathf.Atan2(distanceZ, distanceX) * Mathf.Rad2Deg;
+        Vector3 forward = transform.parent.forward;
+        Vector3 toPlayer = (Player.position - transform.parent.position).normalized;
+        forward.y = 0;
+        toPlayer.y = 0;
+
+        float angulo = Vector3.SignedAngle(forward, toPlayer, Vector3.up);
 
         if (angulo < 0) angulo += 360;
 
-        int spriteAngles = Mathf.RoundToInt((angulo / 45f)) -1;
+        int spriteAngles = Mathf.FloorToInt((angulo + 22.5f) / 45f) % 8;
         spriteRender.sprite = sprites[spriteAngles];
 
         if (Input.GetKeyDown(KeyCode.G))
-            Debug.Log(angulo);
-
-        if (canLookVertical)
+        Debug.Log(angulo);
+        if (!enemyActions.playerIsSeen)
         {
-            transform.LookAt(Player.transform);
-        }
-        else
-        {
-            Vector3 modifiedTarget = Player.position;
-            modifiedTarget.y = transform.position.y; 
+            if (canLookVertical)
+            {
+                transform.LookAt(Player.transform);
+            }
+            else
+            {
+                Vector3 modifiedTarget = Player.position;
+                modifiedTarget.y = transform.position.y;
 
-            transform.LookAt(modifiedTarget);
+                transform.LookAt(modifiedTarget);
+            }
         }
+
     }
 }
